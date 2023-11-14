9f1d4654fa5588cf3f6b578f350f92e243eeb28b
Joaovvb <rm98938@fiap.com.br>
2023-11-13 22:28:13 -0300

    Quarta Rota

---
+++ src/app/Veiculo/page.js
@@ -0,0 +1,14 @@
+"use client"
+import FormularioVeiculo from "../../Components/FormularioVeiculo"
+import HeaderVeiculo from "../../Components/HeaderVeiculo"
+
+function Veiculo(){
+    return(
+        <>
+            <HeaderVeiculo></HeaderVeiculo>       
+            <FormularioVeiculo></FormularioVeiculo>
+        </>
+    )
+}
+
+export default Veiculo
