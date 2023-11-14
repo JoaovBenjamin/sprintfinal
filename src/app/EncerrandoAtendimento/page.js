4789a432b0050348a3073cfd787879828978489a
Joaovvb <rm98938@fiap.com.br>
2023-11-13 22:27:15 -0300

    Segunda rota

---
+++ src/app/EncerrandoAtendimento/page.js
@@ -0,0 +1,10 @@
+import Encerramento from "../../Components/Encerramento"
+function EncerrandoAtendimento(){
+    return(
+        <>
+            <Encerramento></Encerramento>
+        </>
+    )
+}
+
+export default EncerrandoAtendimento
