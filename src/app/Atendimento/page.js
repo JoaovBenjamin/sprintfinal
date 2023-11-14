976e683b2d6616d2cd6d5f495cbb6f85db6f5b98
Joaovvb <rm98938@fiap.com.br>
2023-11-13 22:26:50 -0300

    Criação de rotas e um arquivo

---
+++ src/app/Atendimento/page.js
@@ -0,0 +1,15 @@
+"use client"
+import Formulario from "../../Components/Formulario"
+import HeaderFormulario1 from "../../Components/HeaderFormulario1"
+
+function IniciandoAtendimento(){
+    return(
+        <>
+            <HeaderFormulario1></HeaderFormulario1>
+            <Formulario></Formulario>
+            
+        </>
+    )
+}
+
+export default IniciandoAtendimento
